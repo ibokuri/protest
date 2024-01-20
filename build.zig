bae398b701e763ef18480aa3cfcca103716996de
Danh Phan <input@danh.computer>
2024-01-20 11:18:17 -0600

    build: Organize build.zig

--- build.zig
+++ build.zig
@@ -5,88 +5,45 @@ const package_path = "src/protest.zig";
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
-    const mode = b.standardOptimizeOption(.{});
+    const optimize = b.standardOptimizeOption(.{});
 
+    // Package module.
     _ = b.addModule(
         package_name,
         .{ .root_source_file = .{ .path = package_path } },
     );
 
-    tests(b, target, mode);
-    docs(b, target, mode);
-}
+    // Tests
+    {
+        const test_step = b.step("test", "Run tests");
+        const test_require_step = b.step("test-require", "Run require tests");
 
-fn tests(
-    b: *std.Build,
-    target: std.Build.ResolvedTarget,
-    mode: std.builtin.OptimizeMode,
-) void {
-    const test_step = b.step("test", "Run tests");
-
-    // Allow a test filter to be specified.
-    //
-    // ## Example
-    //
-    // ```
-    // $ zig build test -- "foo - bar"
-    // ```
-    if (b.args) |args| {
-        switch (args.len) {
-            0 => unreachable, // UNREACHABLE: b.args is null if no arguments are given.
-            1 => {
-                const cmd = b.addSystemCommand(&[_][]const u8{
-                    "zig",
-                    "test",
-                    "--main-pkg-path",
-                    "src/",
-                    package_path,
-                    "--test-filter",
-                    args[0],
-                });
-
-                test_step.dependOn(&cmd.step);
-
-                return;
-            },
-            else => |len| std.debug.panic("expected 1 argument, found {}", .{len}),
-        }
+        const t_require = b.addTest(.{
+            .name = "require test",
+            .root_source_file = .{ .path = "src/require.zig" },
+            .target = target,
+            .optimize = optimize,
+        });
+        test_require_step.dependOn(&b.addRunArtifact(t_require).step);
+        test_step.dependOn(test_require_step);
     }
 
-    const test_require_step = b.step("test-require", "Run require tests");
+    // Documentation.
+    {
+        const docs_step = b.step("docs", "Build the project documentation");
 
-    // Configure tests.
-    const t_require = b.addTest(.{
-        .name = "require test",
-        .root_source_file = .{ .path = "src/require.zig" },
-        .target = target,
-        .optimize = mode,
-    });
+        const doc_obj = b.addObject(.{
+            .name = "docs",
+            .root_source_file = .{ .path = package_path },
+            .target = target,
+            .optimize = optimize,
+        });
 
-    // Configure module-level test steps.
-    test_require_step.dependOn(&b.addRunArtifact(t_require).step);
-
-    // Configure top-level test step.
-    test_step.dependOn(test_require_step);
-}
-
-fn docs(
-    b: *std.Build,
-    target: std.Build.ResolvedTarget,
-    mode: std.builtin.OptimizeMode,
-) void {
-    const docs_step = b.step("docs", "Build the project documentation");
-
-    const doc_obj = b.addObject(.{
-        .name = "docs",
-        .root_source_file = .{ .path = package_path },
-        .target = target,
-        .optimize = mode,
-    });
-
-    const install_docs = b.addInstallDirectory(.{
-        .source_dir = doc_obj.getEmittedDocs(),
-        .install_dir = .prefix,
-        .install_subdir = "docs/protest",
-    });
-    docs_step.dependOn(&install_docs.step);
+        const install_docs = b.addInstallDirectory(.{
+            .source_dir = doc_obj.getEmittedDocs(),
+            .install_dir = .prefix,
+            .install_subdir = "docs/protest",
+        });
+        docs_step.dependOn(&install_docs.step);
+    }
 }
